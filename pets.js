d7c8eb0d8c14f4cd3f0d5caafc06aacbe47e754b
Steve Witman <stevewitman@gmail.com>
2016-09-11 01:00:12 -0600

    refactored read and create into functions and added update function

--- pets.js
+++ pets.js
@@ -7,26 +7,30 @@ var node = path.basename(process.argv[0]);
 var file = path.basename(process.argv[1]);
 var cmd = process.argv[2];
 
-
-Number.isInteger = Number.isInteger || function(value) {
-  console.log('here');
-  return typeof value === "number" &&
-    isFinite(value) &&
-    Math.floor(value) === value;
-};
-
-
 if (cmd === 'read') {
+  readPet(process.argv[3]);
+} else if (cmd === 'create') {
+  createPet(process.argv[3], process.argv[4], process.argv[5]);
+} else if (cmd === 'update') {
+  updatePet(process.argv[3], process.argv[4], process.argv[5], process.argv[6]);
+} else {
+  console.error(`Usage: ${node} ${file} [read | create | update | destroy]`);
+  process.exit(1);
+}
 
+function isNormalInteger(str) {
+    return /^(0|[1-9]\d*)$/.test(str);
+}
+
+function readPet(index) {
   fs.readFile(petsPath, 'utf8', function(err, data) {
     if (err) {
       throw err;
     }
     var pets = JSON.parse(data);
-    // check for an index parameter
-    var param1 = process.argv[3];
-    if (param1) {
-      if (param1 >= 0 && param1 < pets.length) {
+    console.log('params:', index);
+    if (index) {
+      if (isNormalInteger(index) && param1 >= 0 && param1 < pets.length) {
         pets = pets[param1];
       } else {
         console.error(`Usage: ${node} ${file} read INDEX`);
@@ -35,46 +39,56 @@ if (cmd === 'read') {
     }
     console.log(pets);
   })
-} else if (cmd === 'create') {
-  var param1 = process.argv[3];
-  var param2 = process.argv[4];
-  var param3 = process.argv[5];
-  if (param1 && param2 && param3 && isNormalInteger(param1)) {
-    var pet = {}
-    pet.age = param1;
-    pet.kind = param2;
-    pet.name = param3;
+}
 
+function createPet(age, kind, name) {
+  if (age && kind && name && isNormalInteger(age)) {
+    var pet = {}
+    pet.age = age;
+    pet.kind = kind;
+    pet.name = name;
     fs.readFile(petsPath, 'utf8', function(readErr, data) {
       if (readErr) {
         throw readErr;
       }
       var pets = JSON.parse(data);
-      console.log(pets);
       pets.push(pet)
-      console.log(pets);
-      var petJSON = JSON.stringify(pets);
-      fs.writeFile(petsPath, petJSON, function(writeErr) {
+      var petsJSON = JSON.stringify(pets);
+      fs.writeFile(petsPath, petsJSON, function(writeErr) {
         if (writeErr) {
           throw writeErr;
         }
-        console.log(pet)
       })
+      console.log(pets);
     });
   } else {
     console.error(`Usage: ${node} ${file} create AGE KIND NAME`);
     process.exit(1);
   }
-
-
-
-
-
-} else {
-  console.error(`Usage: ${node} ${file} [read | create | update | destroy]`);
-  process.exit(1);
 }
 
-function isNormalInteger(str) {
-    return /^(0|[1-9]\d*)$/.test(str);
+function updatePet(index, age, kind, name) {
+  fs.readFile(petsPath, 'utf8', function(readErr, data) {
+    if (readErr) {
+      throw readErr;
+    }
+    var pets = JSON.parse(data);
+    if (index && isNormalInteger(index) && index >= 0 && index < pets.length && age && isNormalInteger(age) && kind && name) {
+      var pet = {}
+      pet.age = age;
+      pet.kind = kind;
+      pet.name = name;
+      pets[index] = pet
+      var petsJSON = JSON.stringify(pets);
+      fs.writeFile(petsPath, petsJSON, function(writeErr) {
+        if (writeErr) {
+          throw writeErr;
+        }
+      })
+      console.log(pets);
+    } else {
+      console.error(`Usage: ${node} ${file} update INDEX AGE KIND NAME`);
+      process.exit(1);
+    }
+  });
 }
